4b8c1e59defbc1a0698620bdc313a9b4307580f6
monika2615 <attrimonika15@gmail.com>
2026-07-27 18:02:15 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,17 @@
+#in this coding we use operators for making calculators we use operators 
+n = int(input("enter your first number:"))
+m = int(input("enter your second number:"))
+ans1 = n+m
+print("addition of ", n , "and", m , "is",ans1)
+ans2= n-m
+print("Subtraction of",n , "and", m , "is" , ans2)
+ans3=n*m
+print(f'multiplication of {n}and {m}is {ans3}')
+ans4=n/m
+print(f'division of {n} and {m} is {ans4}')
+ans5 = n%m
+print(f"modulus of {n}and{m}is {ans5}")
+ans6=n**m
+print(f"exponential of {n} and {m}is {ans6}")
+ans7= n//m
+print(f"floor division of {n}and{m} is {ans7}")
